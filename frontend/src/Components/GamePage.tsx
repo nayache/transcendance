572fe3748cbdfff86c611508a1060724cbb04c48
Alan <lan@LAPTOP-SR57LPG7.localdomain>
2023-01-17 16:49:19 +0100

    ajout verif token / page

--- frontend/src/Components/GamePage.tsx
+++ frontend/src/Components/GamePage.tsx
@@ -2,11 +2,12 @@ import React, { useState } from "react"
 import Background from './Background'
 import Baseline from "./Baseline";
 import Playground from "./Playground";
+import { getPrivateDOMElements } from '../Functions/Token_utils'
 import styled from "styled-components";
 
 const GamePage: React.FC = () => {
 
-	return (
+	return getPrivateDOMElements (
 		<React.Fragment>
 			<Background />
 			<Baseline />
