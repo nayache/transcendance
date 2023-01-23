968f7601858af9c77929dc2e652e55ccd67e82d6
alongue <57681564+alongue@users.noreply.github.com>
2023-01-23 16:51:20 +0100

    Update Register.tsx
    Ajustement de la variable domain : rajout du 'http://'

--- frontend/src/Components/Register.tsx
+++ frontend/src/Components/Register.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const Register: React.FC = () => {
 	
-	const domain: string = 'localhost:3042'
+	const domain: string = 'http://localhost:3042'
 	const api42Url: string = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-21591b92686c701d5d9baf9f9d3e877072ef6d82b19483499cf3b942ed6cd5d8&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fregister&response_type=code'
 	const rawUrlParameters: string = window.location.search;
 	const cleanUrlParameters: URLSearchParams = new URLSearchParams(rawUrlParameters);
