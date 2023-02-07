51d6dd97a7e7a0ef3edf77100e22249d20fe5127
Alan <lan@LAPTOP-SR57LPG7.localdomain>
2023-02-07 14:05:57 +0100

    error enum replaced

---
+++ frontend/src/constants/error_constants.ts
@@ -0,0 +1,16 @@
+export enum AboutError {
+	TOKEN,
+	HEADER,
+	USER,
+	AVATAR,
+	PSEUDO
+}
+
+export enum TypeError {
+	UPDATED,
+	EXPIRED,
+	NOT_FOUND,
+	EMPTY, // parametre vide
+	INVALID,
+	DUPLICATED,
+}
